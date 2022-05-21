7133807d031b4d13732ed6e4d0b88952c9d5f8f5
eldarheleg <eldar.heleg@gmail.com>
2022-05-21 17:16:33 +0200

    get metod

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const get_dugme = document.querySelector(".get_button");
+const delete_dugme = document.querySelector(".delete_button");
+const screen = document.querySelector(".screen");
+const post_button = document.querySelector(".post_button");
+const put_button = document.querySelector(".put_button");
+
+const getFunction = async () =>{
+    const podaci = await fetch(
+        "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars"
+    )
+    const gotovi_podaci = await podaci.json();
+    console.log(gotovi_podaci);
+}
+
+get_dugme.addEventListener("click",() => {
+    getFunction();
+});
