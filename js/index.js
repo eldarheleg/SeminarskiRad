3ac3856d2e0e7608c09963046649c329298279c2
eldarheleg <eldar.heleg@gmail.com>
2022-05-22 23:05:11 +0200

    some changes

--- js/index.js
+++ js/index.js
@@ -14,65 +14,82 @@ const getFunction = async () =>{
     for (let i=0; i<gotovi_podaci.length; i++){
         let htmlString = `<div class="auto">
         <img src="${gotovi_podaci[i].imageUrl}" alt="slika"> 
-      </div>
-      <div class="auto-info">
+    </div>
+    <div class="auto-info">
         <h2>${gotovi_podaci[i].name}</h2>
         <h2>${gotovi_podaci[i].manufacturer}</h2>
         <h2>${gotovi_podaci[i].year}</h2>
         <h2>${gotovi_podaci[i].price}</h2>
-      </div>`;
-      screen.insertAdjacentHTML("beforeend", htmlString);
+    </div>`;
+    screen.insertAdjacentHTML("beforeend", htmlString);
     }
 }
 const deleteFunction = async () => {
     //get metoda za dobijanje id-a prvog obroka. Potrebdno u delete metodi.
     const podaci = await fetch(
-      "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars"
+    "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars"
     );
     const gotovi_podaci = await podaci.json();
     const id = gotovi_podaci[0].id;
-  
     //delete fetch ocekuje parametar id kojeg dodajemo na kraj linka
     const odgovor = await fetch(
-      `https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars/${id}`,
-      {
+    `https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars/${id}`,
+    {
         method: "DELETE",
-      }
+    }
     );
     console.log(odgovor); //Odgovor delete
-  };
+};
 
-  const postFunction = async () => {
+const postFunction = async () => {
     const response = await fetch(
-      "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars",
-      {
+    "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars",
+    {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+        "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          id: 10,
-          name: "Passat 5+",
-          manufacturer: "Volkswagen",
-          imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ultimatespecs.com%2Fcar-specs%2FVolkswagen%2F2964%2FVolkswagen-Passat-B55-19-TDI-130-6sp.html&psig=AOvVaw31QV4zteP88Kwn4b-DOq3L&ust=1653337065796000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMDt4tf28_cCFQAAAAAdAAAAABAK",
-          price: 4500,
-          year: 2003
+        id: 10,
+        name: "Passat 5+",
+        manufacturer: "Volkswagen",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ultimatespecs.com%2Fcar-specs%2FVolkswagen%2F2964%2FVolkswagen-Passat-B55-19-TDI-130-6sp.html&psig=AOvVaw31QV4zteP88Kwn4b-DOq3L&ust=1653337065796000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMDt4tf28_cCFQAAAAAdAAAAABAK",
+        price: 4500,
+        year: 2003
         }),
-      }
+    }
     );
-    
     console.log(response);
-  };
-
-
+};
+const putFunction = async () => {
+    const odgovor = await fetch(
+    "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars",
+    {
+        method: "PUT",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        id: 4,
+        name: "Polo",
+        manufacturer: "Volkswagen",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.auto-data.net%2Fen%2Fvolkswagen-polo-iv-9n-facelift-2005-1.4-80hp-8412&psig=AOvVaw3ZJUnLfzWy9DP53GFSRVss&ust=1653339408840000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOi9y7X_8_cCFQAAAAAdAAAAABAO",
+        price: 3500,
+        year: 2005
+        }),
+    }
+    );
+    console.log(odgovor);
+};
 get_dugme.addEventListener("click",() => {
     getFunction();
 });
 delete_dugme.addEventListener("click", () => {
     deleteFunction();
-  });
-  
-  post_button.addEventListener("click", () => {
+});
+post_button.addEventListener("click", () => {
     postFunction();
-  });
-  
+});
+put_button.addEventListener("click", () =>{
+    putFunction();
+})
