76633178da71caa2448e8f68f86f36741d1d0809
eldarheleg <eldar.heleg@gmail.com>
2022-05-22 22:35:21 +0200

    GET POST DELETE

--- js/index.js
+++ js/index.js
@@ -10,8 +10,69 @@ const getFunction = async () =>{
     )
     const gotovi_podaci = await podaci.json();
     console.log(gotovi_podaci);
+
+    for (let i=0; i<gotovi_podaci.length; i++){
+        let htmlString = `<div class="auto">
+        <img src="${gotovi_podaci[i].imageUrl}" alt="slika"> 
+      </div>
+      <div class="auto-info">
+        <h2>${gotovi_podaci[i].name}</h2>
+        <h2>${gotovi_podaci[i].manufacturer}</h2>
+        <h2>${gotovi_podaci[i].year}</h2>
+        <h2>${gotovi_podaci[i].price}</h2>
+      </div>`;
+      screen.insertAdjacentHTML("beforeend", htmlString);
+    }
 }
+const deleteFunction = async () => {
+    //get metoda za dobijanje id-a prvog obroka. Potrebdno u delete metodi.
+    const podaci = await fetch(
+      "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars"
+    );
+    const gotovi_podaci = await podaci.json();
+    const id = gotovi_podaci[0].id;
+  
+    //delete fetch ocekuje parametar id kojeg dodajemo na kraj linka
+    const odgovor = await fetch(
+      `https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    console.log(odgovor); //Odgovor delete
+  };
+  
+  const postFunction = async () => {
+    const response = await fetch(
+      "https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: 10,
+          name: "Passat 5+",
+          manufacturer: "Volkswagen",
+          imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ultimatespecs.com%2Fcar-specs%2FVolkswagen%2F2964%2FVolkswagen-Passat-B55-19-TDI-130-6sp.html&psig=AOvVaw31QV4zteP88Kwn4b-DOq3L&ust=1653337065796000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMDt4tf28_cCFQAAAAAdAAAAABAK",
+          price: 4500,
+          year: 2003
+        }),
+      }
+    );
+    //opcionalno za bolji ispis poruke od servera
+    console.log(response);
+  };
+
 
 get_dugme.addEventListener("click",() => {
     getFunction();
 });
+delete_dugme.addEventListener("click", () => {
+    deleteFunction();
+  });
+  
+  post_button.addEventListener("click", () => {
+    postFunction();
+  });
+  
